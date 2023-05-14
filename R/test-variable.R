3ca7ad7e8063cf9be542485d0c9a944645b605fb
Beatriz Milz <milz.bea@gmail.com>
2023-05-14 19:30:41 -0300

    improve variable check

--- R/test-variable.R
+++ R/test-variable.R
@@ -1,15 +1,28 @@
 test_missing <- function(dataset, col){
+
   missing <-  dataset |>
-    dplyr::filter({{col}} == "NA" | is.na({{col}})) |>
-    dplyr::select(code, reference, doi, {{col}})
+    dplyr::filter(.data[[col]] == "NA" | is.na(.data[[col]])) |>
+    dplyr::select(code, reference, doi, .data[[col]])
 
   if(nrow(missing) > 0){
-    print(paste0("Missing information:", col))
+    print(htmltools::h2(paste0("Missing information: `", col, "`")))
     print(knitr::kable(missing))
+  htmltools::br()
   }
 
-
-  testthat::expect_equal(nrow(missing), 0)
-
-
 }
+
+# test_missing_wider_binary <- function(dataset, cols){
+# browser()
+#   missing <-  dataset |>
+#     dplyr::mutate(sum_vars = sum(dplyr::select(tidyselect::any_of(cols)))) |>
+#     dplyr::filter(sum_vars == "NA" | is.na(sum_vars)) |>
+#     dplyr::select(code, reference, doi, .data[[cols]])
+#
+#   if(nrow(missing) > 0){
+#     print(htmltools::h2(paste0("Missing information: `", cols, "`")))
+#     print(knitr::kable(missing))
+#     htmltools::br()
+#   }
+#
+# }
