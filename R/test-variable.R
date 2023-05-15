4a7e443a80b3cf48d82ebac0cac0b963392f81f3
Beatriz Milz <milz.bea@gmail.com>
2023-05-15 19:14:52 -0300

    data quality check report

--- R/test-variable.R
+++ R/test-variable.R
@@ -12,17 +12,4 @@ test_missing <- function(dataset, col){
 
 }
 
-# test_missing_wider_binary <- function(dataset, cols){
-# browser()
-#   missing <-  dataset |>
-#     dplyr::mutate(sum_vars = sum(dplyr::select(tidyselect::any_of(cols)))) |>
-#     dplyr::filter(sum_vars == "NA" | is.na(sum_vars)) |>
-#     dplyr::select(code, reference, doi, .data[[cols]])
-#
-#   if(nrow(missing) > 0){
-#     print(htmltools::h2(paste0("Missing information: `", cols, "`")))
-#     print(knitr::kable(missing))
-#     htmltools::br()
-#   }
-#
-# }
+
