d38f6a53861017b4e020e492295955fa0b39dfed
Beatriz Milz <milz.bea@gmail.com>
2023-06-09 11:34:55 -0300

    function to fix DOI url

---
+++ R/fix_doi.R
@@ -0,0 +1,19 @@
+fix_doi <- function(doi_url){
+
+  dplyr::case_when(
+    doi_url == "-" ~ NA_character_,
+    doi_url == "NA" ~ NA_character_,
+    stringr::str_starts(doi_url, "doi: ") ~ paste0("https://doi.org/", stringr::str_remove(doi_url, "^doi: ")),
+    stringr::str_starts(doi_url, "doi.org/") ~ paste0("https://", doi_url),
+    stringr::str_starts(doi_url, "dx.doi.org/") ~ paste0("https://", doi_url),
+    stringr::str_starts(doi_url, "hdl.handle.net/") ~ paste0("https://", doi_url),
+    !stringr::str_starts(doi_url, "https://doi.org/") ~ paste0("https://doi.org/", doi_url),
+
+    TRUE ~ doi_url
+  ) |>
+    stringr::str_remove_all("\\.$") |>
+    stringr::str_remove_all("^\\[") |>
+    stringr::str_remove_all("\\]$") |>
+    stringr::str_remove_all(" ")
+}
+
