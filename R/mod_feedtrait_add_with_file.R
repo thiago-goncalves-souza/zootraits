a7968ce5e7aa54ad4e653d4cfbedb19964c75473
Beatriz Milz <milz.bea@gmail.com>
2023-09-12 08:59:03 -0300

    downloading template

--- R/mod_feedtrait_add_with_file.R
+++ R/mod_feedtrait_add_with_file.R
@@ -38,10 +38,11 @@ mod_feedtrait_add_with_file_ui <- function(id){
           collapsible = FALSE,
           width = 12,
           fluidRow(
-            HTML("Fill out the template. Make sure to create one row for each
-               paper. Use the section below in this page as a guide to know
-               what are the
-               options to use in each column. In case you need to
+            HTML("- Fill out the template. <br>
+                 - Make sure to create one row for each paper. <br>
+               - Use the section below in this page as a guide to know
+               what are the options to use in each column. <br>
+               - In case you need to
                add more than one category in a column, make sure to
                separate the content with a semicolon `; `.")
           ),
@@ -52,7 +53,10 @@ mod_feedtrait_add_with_file_ui <- function(id){
               fileInput(
                 inputId = ns("upload_template"),
                 label = "Upload the file",
-                multiple = FALSE
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")
               )
             )
           )
@@ -66,8 +70,44 @@ mod_feedtrait_add_with_file_ui <- function(id){
 #'
 #' @noRd
 mod_feedtrait_add_with_file_server <- function(id){
-  moduleServer( id, function(input, output, session){
+  moduleServer(id, function(input, output, session){
     ns <- session$ns
+
+    output$download_template <- downloadHandler(
+      filename = "feedtraits_template.csv",
+      contentType = "text/csv",
+      content = function(file) {
+
+       template <- tibble::tibble(
+          paper_title = as.character(),
+          paper_doi = as.character(),
+          paper_url = as.character(),
+          paper_author = as.character(),
+          paper_journal = as.character(),
+          paper_year = as.character(),
+          study_scale = as.character(),
+          ecosystem = as.character(),
+          where = as.character(),
+          taxonomic_unit = as.character(),
+          taxonomic_group = as.character(),
+          latitude = as.character(),
+          longitude = as.character(),
+          trait_type = as.character(),
+          intraspecific_data = as.character(),
+          trait_dimension = as.character(),
+          trait_details = as.character(),
+          trait_details_other = as.character(),
+          contributor_name = as.character(),
+          contributor_email = as.character(),
+          contributor_affiliation = as.character(),
+          contributor_orcid = as.character(),
+        ) |>
+         tibble::add_row()
+
+       readr::write_csv(template, file)
+      }
+    )
+
 
   })
 }
