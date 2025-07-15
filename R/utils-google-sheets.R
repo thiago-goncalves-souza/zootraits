f5b0dfd5c2c7b22b1d35928897badc069a472960
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-07-15 09:49:50 -0300

    auth google sheets

--- R/utils-google-sheets.R
+++ R/utils-google-sheets.R
@@ -1,3 +1,15 @@
 auth_google_sheets <- function() {
-  googlesheets4::gs4_auth(path = "inst/token.json")
+  env_var <- Sys.getenv("TOKEN_GOOGLESHEETS_ZOOTRAITS")
+
+  env_var |>
+    stringr::str_trunc(width = 100) |>
+    print()
+
+  if (env_var == "") {
+    stop(
+      "Please set the environment variable TOKEN_GOOGLESHEETS_ZOOTRAITS with the credentials to access the Google Sheet."
+    )
+  }
+
+  googlesheets4::gs4_auth(path = env_var)
 }
