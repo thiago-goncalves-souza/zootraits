f984036cad0f929510632a996bbc394da69ba465
Beatriz Milz <42153618+beatrizmilz@users.noreply.github.com>
2025-07-16 10:28:15 -0300

    print content for token

--- R/utils-google-sheets.R
+++ R/utils-google-sheets.R
@@ -1,7 +1,13 @@
 auth_google_sheets <- function() {
   env_var <- Sys.getenv("TOKEN_GOOGLESHEETS_ZOOTRAITS")
 
+  print("content for env var TOKEN_GOOGLESHEETS_ZOOTRAITS")
   env_var |>
+    stringr::str_trunc(width = 100) |>
+    print()
+
+  print("content for inst/token.json")
+  paste(readLines("inst/token.json"), collapse = "\n") |>
     stringr::str_trunc(width = 100) |>
     print()
 
